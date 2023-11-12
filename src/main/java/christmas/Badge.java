e777be199f392b2f3f5031604ca242185607e728
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-12 23:07:39 +0900

    Fix: 변경 Badge 클래스
    기존 열거형이었던 Badge를 클래스로 전환. "없음" 추가

--- src/main/java/christmas/Badge.java
+++ src/main/java/christmas/Badge.java
@@ -1,21 +1,22 @@
 package christmas;
 
-import java.util.Arrays;
+public class Badge {
+    enum BadgeSort {
+        없음(0),
+        별(5000),
+        트리(10000),
+        산타(20000);
 
-public enum Badge {
-    별(5000),
-    트리(10000),
-    산타(20000);
+        private int discountedPrice;
 
-    private int discountedPrice;
-
-    Badge(int discountedPrice){
-        this.discountedPrice = discountedPrice;
+        BadgeSort(int discountedPrice) {
+            this.discountedPrice = discountedPrice;
+        }
     }
 
-    public Badge getBedge(int discountedPrice){
-        Badge presentation = null;
-        for(Badge badge : Badge.values()){
+    public BadgeSort getBedge(int discountedPrice){
+        BadgeSort presentation = BadgeSort.없음;
+        for(BadgeSort badge : BadgeSort.values()){
             if(badge.discountedPrice >= discountedPrice){
                 presentation = badge; 
             }
