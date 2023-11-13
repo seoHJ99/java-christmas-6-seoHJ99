f5d6f8b12421278823087ba0345fc15c95770a4e
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-13 23:29:01 +0900

    Fix: 고침 날짜 input

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
 //            검증 함수 추가
-        return 0;
+        return Integer.parseInt(input);
     }
 
     public List<String> readOrder() {
