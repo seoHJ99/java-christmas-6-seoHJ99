65d68d697b01de08778d0a85ec29572328b0bccc
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-13 23:26:38 +0900

    Feat: 추가 Day 열거형 필드 Event
    이벤트에 따른 계산을 더 간편하게 하기 위한 추가

--- src/main/java/christmas/DecemberCalendar.java
+++ src/main/java/christmas/DecemberCalendar.java
@@ -6,25 +6,31 @@ import java.util.stream.Collectors;
 
 public class DecemberCalendar {
     enum Day {
-        MONDAY(4, 11, 18, 25),
-        TUESDAY(5, 12, 19, 26),
-        WEDNSEDAY(6, 13, 20, 27),
-        THURSDAY(7, 14, 21, 28),
-        FRIDAY(1, 8, 15, 22, 29),
-        SATURDAY(2, 9, 16, 23, 30),
-        SUNDAY(3, 10, 17, 24, 31);
+        MONDAY(DiscountEvent.DayEvent.평일_할인, 4, 11, 18, 25),
+        TUESDAY(DiscountEvent.DayEvent.평일_할인, 5, 12, 19, 26),
+        WEDNSEDAY(DiscountEvent.DayEvent.평일_할인, 6, 13, 20, 27),
+        THURSDAY(DiscountEvent.DayEvent.평일_할인, 7, 14, 21, 28),
+        FRIDAY(DiscountEvent.DayEvent.주말_할인, 1, 8, 15, 22, 29),
+        SATURDAY(DiscountEvent.DayEvent.주말_할인, 2, 9, 16, 23, 30),
+        SUNDAY(null, 3, 10, 17, 24, 31);
 
+        private DiscountEvent.DayEvent event;
         private List<Integer> dates;
-        private int date;
 
-        Day(int... date) {
+        Day(DiscountEvent.DayEvent event, int... date) {
+            this.event = event;
             this.dates = Arrays.stream(date).boxed().collect(Collectors.toList());
         }
 
         public List<Integer> getDates() {
             return dates;
         }
+
+        public DiscountEvent.DayEvent getEvent() {
+            return event;
+        }
     }
+
     public Day findDay(int date) {
         return Arrays.stream(Day.values())
                 .filter(Calendar -> Calendar.dates.contains(date))
