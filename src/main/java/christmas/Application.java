938e40c93137b4556637976a6bd9003594204858
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-14 00:26:46 +0900

    Feat: 추가. 정상 구동하는 프로그램

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,33 @@
 package christmas;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        int date = inputView.readDate();
+        List<String> orderString = inputView.readOrder();
+        Order order = new Order();
+        Map<Menu, Integer> orderMenu = order.classifyOrder(orderString);
+        order.validateMenuSort(orderMenu);
+        int fullPrice = order.sumAllOrderMenuPrice(orderMenu);
+        DiscountEvent discountEvent = new DiscountEvent(orderMenu, date);
+        discountEvent.validate(fullPrice);
+        OutputView outputView = new OutputView();
+        outputView.outOrder(orderMenu);
+        int a = discountEvent.getSpecialDiscount();
+        int b = discountEvent.getDiscountPerSort();
+        int c = discountEvent.getChristmasDDayDiscount();
+        int d = discountEvent.getChampagnePresentation(fullPrice);
+        Badge badge = new Badge();
+        Badge.BadgeSort badgeSort = badge.getBedge(a + b + c + d);
+        Map<String, Integer> map = discountEvent.getAllDiscountsMap();
+        outputView.outFullPrice(fullPrice);
+        outputView.outPresentation(map);
+        outputView.outBenefits(map);
+        outputView.outSumBenefits(a + b + c + d);
+        outputView.outActualPayment(fullPrice, map);
+        outputView.outBadge(a + b + c + d);
     }
 }
