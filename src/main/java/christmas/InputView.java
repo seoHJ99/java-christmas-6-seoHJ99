fa176cab04365d94bb95008766d3ff2bbac39743
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-14 00:24:22 +0900

    Feat: 추가 사용자 입력 검증

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -18,9 +18,20 @@ public class InputView {
     public List<String> readOrder() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
-//            검증함수 추가
         List<String> order = new ArrayList<>(Arrays.asList(input.split(",")));
+        //            검증함수 추가
         System.out.println();
         return order;
     }
+
+    private void validateDate(String date) {
+        if (!date.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
+        }
+        int dateInt = Integer.parseInt(date);
+        if (dateInt < 1 || dateInt > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+    
 }
