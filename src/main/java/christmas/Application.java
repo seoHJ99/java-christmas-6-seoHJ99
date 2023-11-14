b6aa923744f8e969c4c2962cc38de222cd1182da
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-14 21:47:49 +0900

    Feat: 기능 개발 완료

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,33 +1,23 @@
 package christmas;
 
-import java.util.List;
 import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        int date = inputView.readDate();
-        List<String> orderString = inputView.readOrder();
-        Order order = new Order();
-        Map<Menu, Integer> orderMenu = order.classifyOrder(orderString);
-        order.validateMenuSort(orderMenu);
+        OutputView outputView = new OutputView();
+        int date = inputView.infiniteDateInputsUntilRight();
+        Order order = inputView.infiniteOrderInputUntilRight();
+        Map<Menu, Integer> orderMenu = order.getOrder();
         int fullPrice = order.sumAllOrderMenuPrice(orderMenu);
         DiscountEvent discountEvent = new DiscountEvent(orderMenu, date);
-        discountEvent.validate(fullPrice);
-        OutputView outputView = new OutputView();
+        Map<String, Integer> allDiscounts = discountEvent.validateAndGetAllDiscount(fullPrice);
+        int sumBenefits = discountEvent.getAllDiscountPrice(allDiscounts);
         outputView.outOrder(orderMenu);
-        int a = discountEvent.getSpecialDiscount();
-        int b = discountEvent.getDiscountPerSort();
-        int c = discountEvent.getChristmasDDayDiscount();
-        int d = discountEvent.getChampagnePresentation(fullPrice);
-        Badge badge = new Badge();
-        Badge.BadgeSort badgeSort = badge.getBedge(a + b + c + d);
-        Map<String, Integer> map = discountEvent.getAllDiscountsMap();
         outputView.outFullPrice(fullPrice);
-        outputView.outPresentation(map);
-        outputView.outBenefits(map);
-        outputView.outSumBenefits(a + b + c + d);
-        outputView.outActualPayment(fullPrice, map);
-        outputView.outBadge(a + b + c + d);
+        outputView.outAllAboutDiscount(allDiscounts);
+        outputView.outSumBenefits(sumBenefits);
+        outputView.outActualPayment(fullPrice, allDiscounts);
+        outputView.outBadge(sumBenefits);
     }
 }
